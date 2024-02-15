3b811e293ded5c5ad2220112155dd0afccd0e0af
bouzagui <badrbouzagui@gmail.com>
2024-02-15 14:03:44 +0000

    done

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdarg.h>
+
+#endif
