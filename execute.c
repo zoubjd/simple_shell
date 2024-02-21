ad8b42c6d18187557b9aeec3e6072e0737a749a4
zoubjd <zouhairbajdouri19@gmail.com>
2024-02-22 00:55:25 +0100

    pushing

--- execute.c
+++ execute.c
@@ -1,31 +1,32 @@
 #include "shell.h"
 
-int my_execute(char **command, __attribute__ ((unused)) char **argv, __attribute__ ((unused)) int idex)
+int my_execute(char **command, __attribute__ ((unused)) char **argv,
+__attribute__ ((unused)) int idex)
 {
-    pid_t child_pid;
-    int status;
+	pid_t child_pid;
+	int status;
 
-    if (command == NULL || command[0] == NULL)
-    {
-    return (0);
-    }
+	if (command == NULL || command[0] == NULL)
+	{
+	return (0);
+	}
 
-    child_pid = fork();
+	child_pid = fork();
 
-    if (child_pid == -1)
-    {
-    perror("fork");
-    return (-1);
-    }
-    else if (child_pid == 0)
-    {
-    execvp(command[0], command);
-    perror("execvp");
-    _exit(EXIT_FAILURE);
-    }
-    else
-    {
-    waitpid(child_pid, &status, 0);
-    return WEXITSTATUS(status);
-    }
+	if (child_pid == -1)
+	{
+	perror("fork");
+	return (-1);
+	}
+	else if (child_pid == 0)
+	{
+	execve(command[0], command, NULL);
+	perror("execve");
+	_exit(EXIT_FAILURE);
+	}
+	else
+	{
+	waitpid(child_pid, &status, 0);
+	return (WEXITSTATUS(status));
+	}
 }
--- main.c
+++ main.c
@@ -2,36 +2,31 @@
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-    char *line = NULL;
-    char **command = NULL;
-    int status = 0;
+char *line = NULL;
+char **command = NULL;
+int status = 0;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-        {
-            write(STDIN_FILENO, "$ ", 2);
-        }
-
-        line = read_line();
-        if (line == NULL)
-        {
-            if (isatty(STDIN_FILENO))
-            {
-                write(STDOUT_FILENO, "\n", 1);
-            }
-            return (status);
-        }
-        else
-        {
-            command = tokens(line);
-            status = my_execute(command, argv, status);
-            if (command == NULL)
-            {
-                free(line);
-                free(command);
-            }
-        }
-    }
-    return (0);
+while (1)
+{
+line = read_line();
+if (line == NULL)
+{
+if (isatty(STDIN_FILENO))
+{
+write(STDOUT_FILENO, "\n", 1);
+}
+return (status);
+}
+else
+{
+command = tokens(line);
+status = my_execute(command, argv, status);
+if (command == NULL)
+{
+free(line);
+free(command);
+}
+}
+}
+return (0);
 }
